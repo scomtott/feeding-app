6109193771b0a1beca221caf7482374eb4ac185e
Tom Scott <tom.tomscott@gmail.com>
2026-02-01 13:46:00 +0000

    add missing file to 'add date filtering to queries'

--- spring-boot/src/main/java/com/example/springboot/persistence/PumpingEntryRepository.java
+++ spring-boot/src/main/java/com/example/springboot/persistence/PumpingEntryRepository.java
@@ -2,9 +2,16 @@ package com.example.springboot.persistence;
 
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.example.springboot.models.PumpingEntry;
 
 @Repository
 public interface PumpingEntryRepository extends JpaRepository<PumpingEntry, Long> {
+    @Query("SELECT p FROM PumpingEntry p WHERE p.date BETWEEN :startDate AND :endDate ORDER BY p.date ASC, p.time ASC")
+    List<PumpingEntry> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
